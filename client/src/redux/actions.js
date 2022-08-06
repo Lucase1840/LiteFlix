8818ac01c9f452507038080114df1b2e82438254
Lucas <l.ucase1840@gmail.com>
2022-08-06 11:47:08 -0300

    dependancies fixed on client, and bad required variable

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -7,7 +7,7 @@ process.env.NODE_ENV === "development" ? URL = "http://localhost:3001" : URL = "
 export const getMovies = () => {
   return function (dispatch) {
     return axios.get(`${URL}`)
-      .then(resp => dispatch({ type: GET_PRODUCTS, payload: resp.data }))
+      .then(resp => dispatch({ type: GET_MOVIES, payload: resp.data }))
       .catch(error => console.log('Action error in getProducts: ', error))
   }
 };
