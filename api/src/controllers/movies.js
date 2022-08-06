c6156e87523c5f368a48243192861d802dd7b422
Lucas <l.ucase1840@gmail.com>
2022-08-06 12:10:39 -0300

    getMovies from external API fixed and working

--- api/src/controllers/movies.js
+++ api/src/controllers/movies.js
@@ -1,20 +1,34 @@
 const axios = require('axios');
 const { API_KEY } = process.env;
-// const { Op } = require("sequelize");
 const { Movies } = require("../db.js");
+// const { Op } = require("sequelize");
 
 async function getMovies(req, res) {
     console.log("Hola, vengo a traer movies")
     try {
-        let mainMovie = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=6f26fd536dd6192ec8a57e94141f8b20`);
+        let highlightedMovie = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=6f26fd536dd6192ec8a57e94141f8b20`);
         let popularMoviesData = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=6f26fd536dd6192ec8a57e94141f8b20`);
         let popularMovies = popularMoviesData.data.results.slice(0, 4)
-        // res.send(mainMovie.data.results[0]);
-        res.send(popularMovies);
+        let response = {
+            highlightedMovie: highlightedMovie.data.results,
+            popularMovies: popularMovies
+        }
+        res.send(response);
     } catch (error) {
         res.send(error)
     }
 };
+
+async function getUserMovies(req, res) {
+    try {
+        let dataBaseMovies = await Movies.findAll();
+        // res.send(dataBaseMovies);
+    } catch (error) {
+        res.send(error)
+    }
+};
+
 module.exports = {
     getMovies,
+    getUserMovies
 };
